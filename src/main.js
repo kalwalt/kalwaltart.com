b86979be47088bbb223c177efbe6b76709573ae9
Walter Perdan <github@kalwaltart.it>
2026-06-16 22:37:09 +0200

    feat(components): implement kw-slideshow and kw-slideshow-slide Lit components

--- src/main.js
+++ src/main.js
@@ -4,3 +4,5 @@ import './main.css';
 // Register Lit Web Components
 import './components/kw-cookie-banner.js';
 import './components/kw-carousel.js';
+import './components/kw-slideshow.js';
+import './components/kw-slideshow-slide.js';
